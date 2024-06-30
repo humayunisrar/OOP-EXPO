792bbc2f62734428f0dca577a0aaa4b9f3e7dad1
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-06-30 15:47:25 +0500

    .

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Pressable,Image } from 'react-native'
+import { View, Pressable,Image } from 'react-native'
 
 import styles from './screenheader.style'
 
